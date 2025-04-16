e00960bf26c6fbcb00bd60a1a28c398c94a152f7
Rafly Irham Safatulloh <raflyirham1005@gmail.com>
2025-04-16 23:39:13 +0700

    fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -4,13 +4,17 @@ import { NextResponse, NextRequest } from "next/server";
 // export { auth as middleware } from "@/auth";
 
 const publicRoutes = ["/auth/signin", "/auth/signup"];
-const protectedRoutes = ["/dashboard/**"];
+const protectedRoutes = ["/dashboard"];
 
 export async function middleware(request: NextRequest) {
   const isLoggedIn = await auth();
 
-  const isPublicRoute = publicRoutes.includes(request.nextUrl.pathname);
-  const isProtectedRoute = protectedRoutes.includes(request.nextUrl.pathname);
+  const isPublicRoute = publicRoutes.some((route) =>
+    request.nextUrl.pathname.startsWith(route)
+  );
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    request.nextUrl.pathname.startsWith(route)
+  );
 
   if (isPublicRoute && isLoggedIn) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
